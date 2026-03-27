80d18d2fc437aec4b6e2047600666aa82853101e
Natsy Bouiti <natsy.bouiti@gmail.com>
2026-03-27 17:37:44 +0100

    feat: ajout endpoint GET /patients/subscription

--- src/routes/patient.routes.js
+++ src/routes/patient.routes.js
@@ -2,13 +2,12 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-// On demande à Node de nous dire exactement où il cherche
 const cheminCerveau = path.join(__dirname, '..', 'controllers', 'patient.controller.js');
 console.log('🔎 Bolamu cherche le fichier ici :', cheminCerveau);
 
-// On essaie de charger le contrôleur avec le chemin complet
-const { registerPatient } = require(cheminCerveau);
+const { registerPatient, getSubscription } = require(cheminCerveau);
 
 router.post('/register', registerPatient);
+router.get('/subscription', getSubscription);
 
 module.exports = router;
