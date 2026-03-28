58727c18be974bcbd82d410e6e0666263f7fc4e4
Natsy Bouiti <natsy.bouiti@gmail.com>
2026-03-28 02:23:27 +0100

    fix: chemin middleware corrigé ../middleware

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const pool = require('./config/db');
-const authMiddleware = require('./middleware/auth.middleware');
+const authMiddleware = require('../middleware/auth.middleware');
 
 const app = express();
 
