6a2d207697a996143a16cb26533dacd0dd9ec9fe
Natsy Bouiti <natsy.bouiti@gmail.com>
2026-03-31 10:23:43 +0200

    fix: require db correct dans momo.routes

--- src/routes/momo.routes.js
+++ src/routes/momo.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { verifyToken } = require('../../middleware/auth.middleware');
-const db = require('../config/database');
+const db = require('../config/db');
 const { v4: uuidv4 } = require('uuid');
 
 const MOMO_BASE_URL = process.env.MOMO_BASE_URL || 'https://sandbox.momodeveloper.mtn.com';
