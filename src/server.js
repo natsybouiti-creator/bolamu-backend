56b9102e4053a4740713d70a3cc0fb0d515817ff
Natsy Bouiti <natsy.bouiti@gmail.com>
2026-04-15 02:22:03 +0200

    Fix: Add CORS and correct API path

--- src/server.js
+++ src/server.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const pool = require('./config/db');
-
+const cors = require('cors');
 const app = express();
 
 // ============================================================
 // 1. MIDDLEWARES
 // ============================================================
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
