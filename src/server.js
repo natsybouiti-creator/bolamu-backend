da4b8a716ae7a7cac1d568bed66d702b2b369d3b
Natsy Bouiti <natsy.bouiti@gmail.com>
2026-04-24 04:31:13 +0200

    test: route debug-sentry

--- src/server.js
+++ src/server.js
@@ -130,7 +130,14 @@ app.get('/urgence', (req, res) => {
 });
 
 // ============================================================
-// 6. ROUTE 404
+// 6. DEBUG SENTRY
+// ============================================================
+app.get('/debug-sentry', (req, res) => {
+    throw new Error('Test Sentry Bolamu — fonctionne !');
+});
+
+// ============================================================
+// 7. ROUTE 404
 // ============================================================
 app.use((req, res) => {
     res.status(404).json({ success: false, message: 'Route introuvable' });
