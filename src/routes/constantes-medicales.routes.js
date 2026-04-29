247d2eb13f21630d0b5fe3722629b5ae94a2a591
Natsy Bouiti <natsy.bouiti@gmail.com>
2026-04-29 05:04:50 +0200

    fix: chemin middleware auth dans constantes-medicales.routes.js

--- src/routes/constantes-medicales.routes.js
+++ src/routes/constantes-medicales.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { authMiddleware } = require('../middlewares/auth.middleware');
+const authMiddleware = require('../middleware/auth.middleware');
 const {
     getConstantes,
     updateConstantesPatient,
